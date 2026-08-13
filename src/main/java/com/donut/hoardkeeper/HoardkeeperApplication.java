a8c35397586d0059ac6a6285096c95fb55d8145d
Donut <antonikrzywicki@gmail.com>
2026-08-13 15:28:59 +0200

    Initial project setup with Spring Boot, JDA 6.5, ProstgreSQL and Flyway

---
+++ src/main/java/com/donut/hoardkeeper/HoardkeeperApplication.java
@@ -0,0 +1,13 @@
+package com.donut.hoardkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoardkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoardkeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donut/hoardkeeper/HoardkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donut.hoardkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoardkeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
